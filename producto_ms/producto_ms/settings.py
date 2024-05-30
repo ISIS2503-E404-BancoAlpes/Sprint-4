033168d73a176fd1996b33a240a8bb1d5342c440
JuanSarS <98623401+JuanSarS@users.noreply.github.com>
2024-05-30 00:32:06 -0500

    Update settings.py

--- producto_ms/producto_ms/settings.py
+++ producto_ms/producto_ms/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'producto_ms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'producto_ms', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
