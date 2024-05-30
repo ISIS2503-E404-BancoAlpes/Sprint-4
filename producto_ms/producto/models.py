a0fe47e30cc42a17c8df9d878a5de46bf7c1acbd
JuanSarS <98623401+JuanSarS@users.noreply.github.com>
2024-05-30 02:48:18 -0500

    Update models.py

--- producto_ms/producto/models.py
+++ producto_ms/producto/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=50, default= None)
     tipo = models.CharField(max_length=50, default= None)
-    fecha= models.DateTimeField(auto_now_add=True)
+    fecha= models.DateTimeField()
     tier = models.CharField(null=True, blank= True,max_length=50)
     def __str__(self): 
         return "%s , %s , %s ,%s"%(self.id,self.nombre,self.fecha,self.tier)
